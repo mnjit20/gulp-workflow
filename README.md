11eee47273eb25a26e5fa810b9aa40a79d405aa6
mnjit20 <mnjit.vrma@gmail.com>
2018-07-02 23:49:38 +0530

    tasks in default task

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# gulp-workflow" 
+
+"#Added 2 differnt tasks added on the default task gulp task: gulp.task('default', ['scripts', 'styles']);"
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 
-
-//gulp.task('default');-
-
-gulp.task('default', function () {
+//Script task
+//uglify
+gulp.task('scripts', function () {
   gulp.src('js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('minjs'));
-
-
+    .pipe(gulp.dest('build/js'));
 
 });
+
+//Styles task
+gulp.task('styles', function () {
+  console.log('style task');
+})
+
+//gulp defualt task
+//passing multiple tasks names as the array, and all are getting executed
+gulp.task('default', ['scripts', 'styles']);
